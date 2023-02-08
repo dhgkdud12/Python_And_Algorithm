524f638ad37821d34ccd9b0cb25de759a622ff2c
hy <75581904+dhgkdud12@users.noreply.github.com>
2023-02-08 18:03:31 +0900

    Python - list max, min 값 구하기

--- Python/dataStructure.py
+++ Python/dataStructure.py
@@ -5,9 +5,26 @@ license = True # boolean
 # 할당 연산자 =
 # 변수가 할당된 객체의 주솟값을 '가리키게' 됨
 
-# 리스트
+# list
 user1 = ['sandara', 14, True]
 
-# 딕셔너리
+# dictionary
 user2 = {'name': 'alex', 'age':3, 'license': True}
 
+# ploblem solving
+nums = [3, 1, 4, 2, 9, 6]
+
+# max
+max_num = -1
+for i in nums :
+    if max_num < i :
+        max_num = i
+
+print('최댓값:', max_num)
+
+# min
+min_num = 9999
+for i in nums:
+    if min_num > i:
+        min_num = i
+print('최솟값:', min_num)
